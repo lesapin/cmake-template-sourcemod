93d733ddf59ebc0e504685386d2c075d14f7887e
lesapin <47056259+lesapin@users.noreply.github.com>
2024-01-11 06:34:17 +0200

    Add files via upload

--- extensions/extension.h
+++ extensions/extension.h
@@ -1,12 +1,14 @@
 /**
- * SourceMod CMake Template
- * Copyright (C) 2024 bezdmn
+ * vim: set ts=4 :
+ * =============================================================================
+ * SourceMod Sample Extension
+ * Copyright (C) 2004-2008 AlliedModders LLC.  All rights reserved.
  * =============================================================================
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, version 3.0, as published by the
  * Free Software Foundation.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
@@ -14,74 +16,104 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * As a special exception, AlliedModders LLC gives you permission to link the
+ * code of this program (as well as its derivative works) to "Half-Life 2," the
+ * "Source Engine," the "SourcePawn JIT," and any Game MODs that run on software
+ * by the Valve Corporation.  You must obey the GNU General Public License in
+ * all respects for all other code used.  Additionally, AlliedModders LLC grants
+ * this exception to all derivative works.  AlliedModders LLC defines further
+ * exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
+ * or <http://www.sourcemod.net/license.php>.
+ *
+ * Version: $Id$
  */
 
-#ifndef _INCLUDE_SOURCEMOD_SIGNALS_EXTENSION_PROPER_H_
-#define _INCLUDE_SOURCEMOD_SIGNALS_EXTENSION_PROPER_H_
+#ifndef _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
+#define _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
 
 /**
- *  @file		extension.h
- *  @brief		Main definitions file of the extension.
+ * @file extension.h
+ * @brief Sample extension code header.
  */
 
 #include "smsdk_ext.h"
-#include "signal.h"
 
-enum ErrCodes
-{
-    NoError = 0,
-    Error,
-    SigactionError,
-    SAHandlerError,
-    ForwardError,
-    FuncCountError,
-    CallbackError
-};
 
-class SignalForwards : public SDKExtension
+/**
+ * @brief Sample implementation of the SDK Extension.
+ * Note: Uncomment one of the pre-defined virtual functions in order to use it.
+ */
+class Sample : public SDKExtension
 {
 public:
-    /**
-     *  @brief	Initialize Signals extension.
-     */
-    virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	/**
+	 * @brief This is called after the initial loading sequence has been processed.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlen	Size of error message buffer.
+	 * @param late		Whether or not the module was loaded after map load.
+	 * @return			True to succeed loading, false to fail.
+	 */
+	//virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	
+	/**
+	 * @brief This is called right before the extension is unloaded.
+	 */
+	//virtual void SDK_OnUnload();
 
-    /**
-     *  @brief	Cleanup and release all the forwards.
-     */
-    virtual void SDK_OnUnload();
+	/**
+	 * @brief This is called once all known extensions have been loaded.
+	 * Note: It is is a good idea to add natives here, if any are provided.
+	 */
+	//virtual void SDK_OnAllLoaded();
+
+	/**
+	 * @brief Called when the pause state is changed.
+	 */
+	//virtual void SDK_OnPauseChange(bool paused);
+
+	/**
+	 * @brief this is called when Core wants to know if your extension is working.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlen	Size of error message buffer.
+	 * @return			True if working, false otherwise.
+	 */
+	//virtual bool QueryRunning(char *error, size_t maxlen);
+public:
+#if defined SMEXT_CONF_METAMOD
+	/**
+	 * @brief Called when Metamod is attached, before the extension version is called.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @param late			Whether or not Metamod considers this a late load.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
+
+	/**
+	 * @brief Called when Metamod is detaching, after the extension version is called.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodUnload(char *error, size_t maxlen);
+
+	/**
+	 * @brief Called when Metamod's pause state is changing.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param paused		Pause state being set.
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlen);
+#endif
 };
 
-/**
- *  @brief	Native functions exposed to plugins
- */
-cell_t CreateHandler(IPluginContext* pContext, const cell_t* params);
-
-/**
- *  @brief	Native functions exposed to plugins
- */
-cell_t RemoveHandler(IPluginContext* pContext, const cell_t* params);
-
-/**
- *  @brief	SigAction specifies the signal handler in a sigaction struct where the SA_SIGINFO flag is set.
- *  Only functions that are considered async-signal-safe should be called from within a signal handler.
- * 
- *  Calling an IForwardManager->Execute() MIGHT be reentrant and therefore async-signal-safe
- *  ...\sourcemod\sourcepawn\vm\scripted-invoker.cpp : ScriptedInvoker::Execute(cell_t* result)
- *  and ScriptedInvoker::Invoke(cell_t* result) seem to imply so, also
- *  ...\sourcemod\core\logic\ForwardSys.cpp : CForward::Execute(cell_t *result, IForwardFilter *filter)
- * 
- *  Are HeapAlloc and HeapPop async-safe? If number of params is kept zero then these should never be called
- * 
- *  @param signal       Numeric value of the signal invoking the handler
- *  @param info	        Structure containing further information about the signal
- *  @param ucontext     Pointer to a ucontext_t struct containing user-space stack information
- */
-void SigAction(int signal, siginfo_t* info, void* ucontext);
-
-/* Helper functions*/
-
-int SetSAHandler(int signal);
-int ResetSAHandler(int signal);
-
-#endif // _INCLUDE_SOURCEMOD_SIGNALS_EXTENSION_PROPER_H_
+#endif // _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
